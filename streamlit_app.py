c6373100cb314be34c8d11283358755734633904
Muralitharan S <58542286+Muralitharan2789@users.noreply.github.com>
2022-08-02 19:45:56 +0530

    Update streamlit_app.py
    imported pandas library to create a data frame from CSV file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
-
-import streamlit
+import pandas
+import streamlit 
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -8,3 +11,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avcoda Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
